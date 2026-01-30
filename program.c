ddf68a4c3437027c9fa4fc5bcb627c5ba3391942
Enzo <enzohakim2008@gmail.com>
2026-01-30 09:01:31 +0200

    Added zoom and camera movement to the program. Optimized it to run smoothly . Fixed some of the unoptimized code

--- program.c
+++ program.c
@@ -12,6 +12,30 @@
 #define HEIGHT 600
 #define COLOR_WHITE 0xFFFFFFFF
 #define COLOR_BLACK 0x000000
+#define cam_x 0.0
+#define cam_y 0.0
+#define cam_zoom 1.0f
+
+typedef struct{
+	float x,y;
+	float zoom;
+	int w, h;
+} Camera;
+
+Camera cam = {cam_x, cam_y, cam_zoom, WIDTH, HEIGHT};
+
+
+void DrawRect(SDL_Renderer* renderer,Camera* cam, SDL_FRect* worldRect){
+	SDL_FRect screenRect;
+    	screenRect.x = (worldRect->x - cam->x) * cam->zoom + (cam->w / 2.0f);
+   	screenRect.y = (worldRect->y - cam->y) * cam->zoom + (cam->h / 2.0f);
+    
+	screenRect.w = worldRect->w * cam->zoom;
+    	screenRect.h = worldRect->h * cam->zoom;
+
+    	SDL_RenderFillRectF(renderer, &screenRect);
+}
+
 int main() {
     SDL_Init(SDL_INIT_VIDEO);
     TTF_Init();
@@ -22,42 +46,56 @@ int main() {
         return 1;
     }
     SDL_Color textColor = { 255, 255, 255, 255 }; // White
-    SDL_Window *window = SDL_CreateWindow("Application", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
-    SDL_Surface *surface = SDL_GetWindowSurface(window);
-
+    SDL_Window *window = SDL_CreateWindow("Application", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0); 
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED); 
     bool isRunning = true;
     SDL_Event event;
     Uint64 last_time = SDL_GetPerformanceCounter();
     double fps = 0.0;
 
-    while (isRunning) {
+while (isRunning) {
         Uint64 current_time = SDL_GetPerformanceCounter();
-        double delta_time = (double)((current_time - last_time) / (double)SDL_GetPerformanceFrequency());
+        double delta_time = (double)(current_time - last_time) / SDL_GetPerformanceFrequency();
         last_time = current_time;
-        if (delta_time > 0.0) {
-            fps = 1.0 / delta_time;
-        }
+        if (delta_time > 0.0) fps = 1.0 / delta_time;
 
         while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                isRunning = false;
+            if (event.type == SDL_QUIT) isRunning = false;
+
+            if (event.type == SDL_MOUSEWHEEL) {
+                if (event.wheel.y > 0) cam.zoom *= 1.1f;
+                else if (event.wheel.y < 0) cam.zoom /= 1.1f;
             }
         }
 
-        SDL_FillRect(surface, NULL, COLOR_BLACK); // Clear screen 
-        SDL_Rect rect = {200, 200, 200, 200};
-        SDL_FillRect(surface, &rect, COLOR_WHITE);
+        const Uint8* state = SDL_GetKeyboardState(NULL);
+        float speed = 400.0f * (float)delta_time; // 400 pixels per second
+        
+        if (state[SDL_SCANCODE_LEFT])  cam.x -= speed / cam.zoom;
+        if (state[SDL_SCANCODE_RIGHT]) cam.x += speed / cam.zoom;
+        if (state[SDL_SCANCODE_UP])    cam.y -= speed / cam.zoom;
+        if (state[SDL_SCANCODE_DOWN])  cam.y += speed / cam.zoom;
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Black
+        SDL_RenderClear(renderer);
+
+        SDL_FRect worldBox = {200, 200, 200, 200};
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // White
+        DrawRect(renderer, &cam, &worldBox);
+
         char fps_text[32];
         snprintf(fps_text, sizeof(fps_text), "FPS: %.2f", fps);
-        SDL_Surface* textSurface = TTF_RenderText_Solid(font, fps_text, textColor);
-        if (textSurface) {
-            SDL_Rect textRect = { 10, 10, textSurface->w, textSurface->h };
-            SDL_BlitSurface(textSurface, NULL, surface, &textRect);
-            SDL_FreeSurface(textSurface);
+        SDL_Surface* textSurf = TTF_RenderText_Solid(font, fps_text, textColor);
+        if (textSurf) {
+            SDL_Texture* textTex = SDL_CreateTextureFromSurface(renderer, textSurf);
+            SDL_Rect textRect = {10, 10, textSurf->w, textSurf->h};
+            SDL_RenderCopy(renderer, textTex, NULL, &textRect);
+            SDL_FreeSurface(textSurf);
+            SDL_DestroyTexture(textTex);
         }
-        SDL_UpdateWindowSurface(window);
-    }
 
+        SDL_RenderPresent(renderer);
+    }
     TTF_CloseFont(font);
     TTF_Quit();
     SDL_DestroyWindow(window);
